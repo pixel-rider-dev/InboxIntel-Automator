e9b3994b135125b538e6ba344dc6e5823ca83150
Usman <heart.uk6777@gmail.com>
2026-05-17 15:39:46 +0500

    fixed database path for render

--- backend/config.py
+++ backend/config.py
@@ -7,5 +7,5 @@ load_dotenv(os.path.join(basedir, '../.env'))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard-to-guess-string'
     # SQLite database jo root ke 'database' folder me save hogi
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '../database/app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
